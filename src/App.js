0d4615c8630b5393360c4a7d85735f620926d950
Matthew Keil <malibuss67@gmail.com>
2019-01-13 19:48:50 -0500

    setting up react with router using js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
 import './App.css';
+
+import {Recipes} from './components';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header"></header>
+        <main className="App-main">
+          <Router class="App-content">
+            <Route path="/" exact component={Recipes} />
+          </Router>
+        </main>
       </div>
     );
   }
---
+++ src/components/Recipes/Recipes.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+import './Recipes.css';
+
+export const Recipes = (props) => <h1>Recipes</h1>;
