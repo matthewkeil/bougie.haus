34380ec123f359620c2c53441ec0bce1d6ef0107
Matthew Keil <malibuss67@gmail.com>
2019-01-16 21:57:18 -0500

    starting to understand responsive design

---
+++ src/components/Recipes/Hero.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import styles from './Hero.module.css';
+
+const Hero = ({hero}) => {
+
+    const actions = [{
+        text: 'full recipe',
+        url: '/recipes/' + hero.shortid
+    }];
+
+    if (hero.video) actions.push({
+        text: 'watch how',
+        url: `/recipes/${hero.shortid}/videos/watch`
+    });
+
+    function stringifyMonth(index) {
+        switch(index) {
+            case 0:
+                return 'Jan';
+            case 1:
+                return 'Feb';
+            case 3:
+                return 'Apr';
+            case 4:
+                return 'May';
+            case 5:
+                return 'Jun';
+            case 6:
+                return 'Jul';
+            case 7:
+                return 'Aug';
+            case 8:
+                return 'Sep';
+            case 9:
+                return 'Oct';
+            case 10:
+                return 'Nov';
+            case 11:
+                return 'Dec';
+            default:
+                return 'Mar';
+        }
+    }
+
+    function stringifyDay(digit) {
+        if (digit < 10) {
+            return '0' + digit;
+        }
+        return `${digit}`;
+    }
+
+    function getToday(date) {
+        
+        const today = date instanceof Date 
+            ? date
+            : Number.isFinite(date)
+                ? new Date(date) 
+                : new Date();
+                
+        const m = today.getMonth();
+        const d = today.getDate();
+        const y = today.getFullYear();
+    
+        return `${stringifyMonth(m)} ${stringifyDay(d)}, ${y}`;
+    }
+
+    const Overlay = () => hero.image || hero.video
+        ? <div className={styles.overlay}>
+            <Link to="">
+                {hero.video
+                  ? <h1>this is where the video goes</h1>
+                  : <img src={hero.image.src} alt={!!hero.image.title ? hero.image.title : 'hero image'} className={styles.image}/>
+                }
+            </Link>
+        </div>
+        : null;
+
+    return (<article className={styles.card}>
+        {/* <Overlay /> */}
+        <div className={styles.content}>
+            <span className={styles.date}>{getToday(hero.date)}</span>
+            <Link to="" style={{textDecoration: 'none'}}>
+                <h2 className={styles.name}>{hero.name}</h2>
+            </Link>
+            <span className={styles.description}>{hero.description}</span>
+        </div>
+        <ul className={styles.actions}>
+        {actions.map((action, key) => (
+            <li key={key} className={styles.actionItem}>
+                <Link to={action.url} className={styles.link}>{action.text}</Link>
+            </li>
+        ))}
+        </ul>
+    </article>);
+};
+
+
+
+export default (Hero);
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardMedia from '@material-ui/core/CardMedia';
-import { withStyles } from '@material-ui/core/styles';
+import Hero from './Hero';
 
 
 const RecipeListItem = ({recipe}) => {
@@ -26,82 +19,6 @@ const RecipeListItem = ({recipe}) => {
     );
 };
 
-const heroStyles = theme => {
-    console.log(theme);
-
-    return ({
-        card: {
-            paddingLeft: '8rem',
-            paddingRight: '8rem',
-            paddingTop: '8rem',
-            paddingBottom: '6rem',
-        },
-        content: {
-            textAlign: 'center',
-            margin: 0,
-            padding: 0,
-            color: theme.palette.primary.dark
-        },
-        date: {
-            fontSize: '1rem',
-            padding: 0,
-            marginTop: 0,
-            marginLeft: 0,
-            marginRight: 0,
-            marginBottom: '4rem',
-            color: 'inherit',
-        },
-        name: {
-            fontWeight: 900,
-            color: 'inherit'
-        },
-        slug: {
-            color: 'inherit',
-        }
-    });
-};
-
-const Hero = withStyles(heroStyles)(({hero, classes}) => {
-
-    function stringify(digit) {
-        if (digit < 10) {
-            return '0' + digit;
-        }
-        return `${digit}`;
-    }
-
-    function getToday(date) {
-        const today = date instanceof Date ? date : new Date();
-        const d = today.getDate();
-        const m = today.getMonth() + 1;
-        const y = today.getFullYear();
-    
-        return `${stringify(m)} / ${stringify(d)} / ${y}`;
-    }
-
-    return (<Card className={classes.card}>
-        <CardActionArea>
-            <CardContent className={classes.content}>
-                <Typography className={classes.date}>{getToday(hero.date)}</Typography>
-                <Typography variant="h2" className={classes.name}>
-                    <Link to="" style={{textDecoration: 'none'}}>{hero.name}</Link>
-                </Typography>
-                <Typography variant="h4" className={classes.slug}>{hero.slug}</Typography>
-            </CardContent>
-            {!!hero.image 
-                ? <CardMedia>
-                    <Link to="">
-                        <img src={hero.image.url} alt={hero.image.title} />
-                    </Link>
-                </CardMedia>
-                : null}
-        </CardActionArea>
-        <CardActions>
-            <Button>Full Recipe</Button>
-        </CardActions>
-    </Card>);
-});
-
 class Recipes extends Component {
 
     _recipes = [{
@@ -110,7 +27,11 @@ class Recipes extends Component {
         name: 'Masala Chai',
         slug: 'spiced milk tea from india',
         description: 'Masala chai is a delicately balanced tea that highlights the subtle complexities of Indian cuisine.  This creamy brew is perfumed with spicy cinnamon, floral cardamom and fragrant clove and anise that unite in your mouth in an explosion of flavor',
-        picture: 'url',
+        image: {
+            src: 'https://cdn.bougie.haus/images/recipes/eD8Ds204/masala_chai.jpg',
+            title: 'masala chai spices',
+        },
+        video: {},
         versions: [{
             ingredients: [],
             process: []
@@ -146,7 +67,7 @@ class Recipes extends Component {
         return (
             <div className="main">
                 { !!hero ? <Hero hero={hero} /> : null }
-                <section className="posts">
+                <section className="recipes">
                     {this._recipes.map((recipe, key) => {
                         if (recipe.shortid !== hero.shortid) {
                                 return <RecipeListItem key={key} recipe={recipe}/>
@@ -159,5 +80,6 @@ class Recipes extends Component {
         );
     }
 }
+
 
 export default Recipes;
