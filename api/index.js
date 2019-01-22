ee84990f98dcc9375150ecff392e31b395fa59f7
Matthew Keil <malibuss67@gmail.com>
2019-01-21 22:20:46 -0500

    added api

--- api/index.js
+++ api/index.js
@@ -4,16 +4,13 @@ const { ApolloServer: Serverless } = require('apollo-server-cloud-functions');
 const { schema, context } = require('./graphql');
 
 if (process.env.NODE_ENV === 'development') {
-    const server = new ApolloServer({schema, context});
-
-    server.listen()
+    new ApolloServer({schema, context})
+        .listen()
         .then(({ url }) => {
             console.log(`Server is running at ${url}`);
         });
 } else {
-    const server = new Serverless({schema, context});
-
-    exports.graphqlBougieHaus = server.createHandler();
+    module.exports = {
+        graphqlBougieHaus: new Serverless({schema, context}).createHandler()
+    };
 }
-
-console.log(exports);
--- index.js
+++
@@ -1,1 +0,0 @@
-exports.graphqlBougieHaus = require('./api');
