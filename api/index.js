0765645e952c3dc74ee8b779925efb06dc3a20ca
Matthew Keil <malibuss67@gmail.com>
2019-03-01 14:05:27 -0500

    setting up heroku

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ const morgan = require('morgan');
 const api = require('./api');
 
 const HOST = process.env.HOST || "bougie.haus";
-const API_PORT = process.env.API_PORT || 80;
+const PORT = process.env.API_PORT || process.env.PORT;
 
 api.use(morgan('dev'));
 
-api.listen(API_PORT, () => {
-    console.log(`api listening on http://${HOST}:${API_PORT}`)
+api.listen(PORT, () => {
+    console.log(`api listening on http://${HOST}:${PORT}`)
 });
